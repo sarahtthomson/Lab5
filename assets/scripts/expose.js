e404f90ec0e49da7c88fc21587737821df0ff9b3
nathanenrici <78573820+nathanenrici@users.noreply.github.com>
2022-10-29 16:57:31 -0700

    Add files via upload

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,20 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.querySelector('#horn-select');
+
+  selectElement.addEventListener('change', (event) => {
+    const image = document.querySelector("img");
+    console.log(`${event.target.value}`)
+    image.src = `assets/images/${event.target.value}.svg`;
+
+    const audio = document.querySelector(".hidden");
+    console.log(`${event.target.value}`)
+    audio.src = `assets/audio/${event.target.value}.mp3`;
+});
+  
+  const audioLevel = document.querySelector('#volume');
+  audioLevel.addEventListener('input', (event) => {
+
+  });
 }
