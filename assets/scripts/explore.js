68809799c13da113ae2b37e1c0c2c4d729bfc6d8
Sarah Thomson <stthomso@ucsd.edu>
2022-10-29 19:18:20 -0700

    make face speek

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,42 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var synth = window.speechSynthesis;
+  const voiceOption = document.querySelector('#voice-select');
+  let voices = [];
+
+  synth.addEventListener('voiceschanged', () => {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceOption.appendChild(option);
+    }
+  });
+
+  const talk = document.querySelector('button');
+
+  talk.addEventListener('click', (event)=> {
+    const text = new SpeechSynthesisUtterance(document.querySelector('textarea').value);
+    text.onend = function(event) {
+      image.src = 'assets/images/smiling.png';
+    }
+    var image = document.querySelector('img');
+
+    const selectedOption = voiceOption.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        text.voice = voices[i];
+        break;
+      }
+    }
+
+    console.log(text.voice);
+
+    synth.speak(text);
+    image.src = 'assets/images/smiling-open.png';
+  });
 }
+
