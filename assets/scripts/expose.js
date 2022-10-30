ab1c290d394dc8ba781356f814962815d7618af1
Sarah Thomson <stthomso@ucsd.edu>
2022-10-29 17:37:11 -0700

    got audio picture working

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -13,10 +13,34 @@ function init() {
     const audio = document.querySelector(".hidden");
     console.log(`${event.target.value}`)
     audio.src = `assets/audio/${event.target.value}.mp3`;
-});
+  });
   
   const audioLevel = document.querySelector('#volume');
-  audioLevel.addEventListener('input', (event) => {
+  
+  audioLevel.addEventListener('input', updateVolume);
 
-  });
+  function updateVolume(e) {
+    console.log(e.target.value);
+
+    const audio = document.querySelector(".hidden");
+    audio.volume = e.target.value/100;
+    const audioImg = document.querySelector('[id=volume-controls] img');
+
+    if(e.target.value == 0) {
+      audioImg.src = 'assets/icons/volume-level-0.svg';
+    }
+
+    else if(e.target.value < 33) {
+      audioImg.src = 'assets/icons/volume-level-1.svg';
+    }
+
+    else if(e.target.value < 67) {
+      audioImg.src = 'assets/icons/volume-level-2.svg';
+    }
+
+    else {
+      audioImg.src = 'assets/icons/volume-level-3.svg';
+    }
+  }
+  
 }
